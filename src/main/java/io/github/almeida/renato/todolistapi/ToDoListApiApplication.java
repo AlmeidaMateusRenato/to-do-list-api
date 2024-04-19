d4d1ae0f08b17c0b908c98c064d709dc05324fa2
renatoa <renatoa@alfaconsultorias.com.br>
2024-04-19 16:51:47 -0300

    Initial commit

---
+++ src/main/java/io/github/almeida/renato/todolistapi/ToDoListApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.almeida.renato.todolistapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/almeida/renato/todolistapi/ToDoListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.almeida.renato.todolistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
